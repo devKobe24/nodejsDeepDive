dbbd4c263fc1422bf522a7f2c8d6d60bf758bdc7
kobe <dev.skyachieve91@gmail.com>
2024-01-22 15:04:49 +0900

    :sparkles: Feat: node.js의 thread 사용 예시.

---
+++ 240122/index.js
@@ -0,0 +1,11 @@
+console.log("1. 녹차 가져다 주기.");
+
+setTimeout(() => {
+  console.log("2. 새우 초밥 주기.");
+},	3000);
+
+setTimeout(() => {
+	console.log("3. 샐러드 주기.");
+},	0);
+
+console.log("4. 음료수 가져다 주기.");
